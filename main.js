2df7dad51ba9ea34591801f02c37aeed2670bd4e
EC2 Default User <ec2-user@ip-172-31-34-154.us-east-2.compute.internal>
2021-07-01 13:09:05 +0000

    stopwatch

---
+++ main.js
@@ -0,0 +1,43 @@
+let startButton = document.getElementById('startButton');
+let stopButton = document.getElementById('stopButton');
+let resetButton = document.getElementById('resetButton');
+let timeArea = document.getElementById('time');
+
+let time = 0;
+let timerId = null;
+
+//console.log(startButton);
+
+startButton.addEventListener('click', (event)=>{
+  timerId = setInterval(() => {
+  time += 100;
+  timeArea.innerText = formatTime(time);
+  },100);
+});
+
+stopButton.addEventListener('click', (event)=>{
+  clearInterval(timerId);
+});
+
+resetButton.addEventListener('click',(event)=>{
+  timeArea.textContent = '00:00:00:000'
+
+
+})
+
+function formatTime(time) {
+  let msec = time % 1000;
+  let tempsec = Math.floor(time / 1000);
+  let sec = tempsec % 60;
+  let min = Math.floor(tempsec / 60);
+  let hour = Math.floor(min / 60);
+  
+  let msStr = msec.toString().padStart(3,'0');
+  let tsecStr = tempsec.toString().padStart(3,'0');
+  let sStr = sec.toString().padStart(2,'0');
+  let mStr = min.toString().padStart(2,'0');
+  let hStr = hour.toString().padStart(2,'0');
+  
+  return  hStr + ':' + mStr+ ':' + sStr + ':' + msStr ;
+  
+}
